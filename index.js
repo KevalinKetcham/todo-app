4eaf26d066a8d77de1f4a5a09abc773b23390b54
Kevalin Ketcham <kevalin.create@gmail.com>
2020-11-03 12:47:04 -0800

    Add index.js page

---
+++ index.js
@@ -0,0 +1,6 @@
+<script>
+    // Create todo: sends a new todo key value pair to the DB
+    // Read todos: requests and displays all the DB todos
+    // Update todo: edits a DB todo
+    // Delete todo: removes a DB todo
+</script>
