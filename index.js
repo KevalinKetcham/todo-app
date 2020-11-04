cb214e60084fe32463e9578d53772ded7f1847a4
Kevalin Ketcham <kevalin.create@gmail.com>
2020-11-04 01:04:50 -0800

    Add MongoDB driver code

--- index.js
+++ index.js
@@ -4,3 +4,13 @@
     // Update todo: edits a DB todo
     // Delete todo: removes a DB todo
 </script>
+
+// MongoDB Driver Code
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://kevalin:<password>@cluster0.ykbkz.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
