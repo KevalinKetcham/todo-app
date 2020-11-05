62a22be444af3596561bddc4c5b5b9fbc0ee0690
Kevalin Ketcham <kevalin.create@gmail.com>
2020-11-05 11:55:59 -0800

    Add read functionality

--- index.js
+++ index.js
@@ -6,47 +6,32 @@ const url = `mongodb+srv://kevalin:${password}@cluster0.ykbkz.mongodb.net/todos?
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected to server")
-  
-  const db = client.db("todos");
-  db.collection('todos').insertOne({
-        todo: 'Pat cow'
-    }).then((result) => {
-        console.log(`Todo inserted: ${result}`);
-  })
+    assert.equal(null, err);
+    console.log("Connected to server");
+    const db = client.db("todos");
+    
+    // Create todo
+    // db.collection('todos').insertOne({
+    //         todo: 'Pat cow'
+    //     }).then((result) => {
+    //         console.log(`Todo inserted: ${result}`);
+    // })
 
-  client.close();
+    // Read todos
+    const readValues = db.collection('todos').find({});
+
+    function displayValues(doc) {
+        console.log(JSON.stringify(doc, null, 4));
+    };
+
+    function readErr(error) {
+        console.log(error)
+    };
+
+    readValues.forEach(displayValues, readErr);
+    
+    // Update todo
+    // Delete todo
+
+    client.close();
 });
-
-
-// Create todo: sends a new todo key value pair to the DB
-// Read todos: requests and displays all the DB todos
-// Update todo: edits a DB todo
-// Delete todo: removes a DB todo
-
-
-
-
-
-
-
-
-
-
-
-
-// MongoDB Driver Code
-
-// Long version
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://kevalin:<password>@cluster0.ykbkz.mongodb.net/<dbname>?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-// Short version
-// mongodb+srv://kevalin:<password>@cluster0.ykbkz.mongodb.net/<dbname>?retryWrites=true&w=majority
