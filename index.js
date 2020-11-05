b59d7b8e630dbd2eeee5d6a936b075e75e73cf04
Kevalin Ketcham <kevalin.create@gmail.com>
2020-11-05 11:21:35 -0800

    Add .gitignore, connect to MongoDB & add create todo functionality

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const db = client.db("test");
+const password = 'Quetwaq`123';
 // Connection URL
-const url = 'mongodb+srv://kevalin:<password>@cluster0.ykbkz.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const url = `mongodb+srv://kevalin:${password}@cluster0.ykbkz.mongodb.net/todos?retryWrites=true&w=majority`;
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
+  console.log("Connected to server")
+  
+  const db = client.db("todos");
+  db.collection('todos').insertOne({
+        todo: 'Pat cow'
+    }).then((result) => {
+        console.log(`Todo inserted: ${result}`);
+  })
+
   client.close();
 });
 
-db.collection('todos').insertOne({
-    todo: 'Pet cow',
-  }).then((result) => {
-    console.log(`Todo inserted: ${result}`);
-})
 
 // Create todo: sends a new todo key value pair to the DB
 // Read todos: requests and displays all the DB todos
 // Update todo: edits a DB todo
 // Delete todo: removes a DB todo
+
+
+
+
+
+
+
+
+
+
+
 
 // MongoDB Driver Code
 
